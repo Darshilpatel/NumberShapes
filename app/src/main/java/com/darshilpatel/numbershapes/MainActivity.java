3fce8fb9f997be175e3b7f143e76af2954d88fa8
Owner <darshil-patel@hotmail.com>
2016-03-09 22:43:55 -0500

    initial

---
+++ app/src/main/java/com/darshilpatel/numbershapes/MainActivity.java
@@ -0,0 +1,15 @@
+package com.darshilpatel.numbershapes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int Bhumi = 53;
+    }
+}
