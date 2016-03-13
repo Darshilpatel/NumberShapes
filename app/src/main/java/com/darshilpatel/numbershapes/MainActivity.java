f3a828239f693812dcc13d91e6f8a7ff7f97b1d6
Owner <darshil-patel@hotmail.com>
2016-03-12 23:13:41 -0500

    checks if it's a triangular number

--- app/src/main/java/com/darshilpatel/numbershapes/MainActivity.java
+++ app/src/main/java/com/darshilpatel/numbershapes/MainActivity.java
@@ -10,5 +10,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        class Number {
+            int number;
+
+            public boolean ifSquare() {
+                
+            }
+
+             public boolean ifTriangle(){
+                 int x = 1;
+                 int triangualNumber = 1;
+
+                 while (triangualNumber < number){
+                     x++;
+                     triangualNumber = triangualNumber + x;
+                 }
+
+                 if (triangualNumber == number){
+                     return true;
+                 } else {
+                     return false;
+                 }
+
+             }
+
+
+        }
+        Number myNumber = new Number();
+        myNumber.number = 6;
+
+
     }
 }
